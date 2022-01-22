644d9e7ecd3c4f771a76e14990016004250d9790
MHFarooqui <farooquihanif02@gmail.com>
2022-01-22 16:17:09 +0530

    changed port env variable name

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get('/api',  (req, res) => res.send({
 }));
 
 app.use(routes);
-let portNumber = process.env.PORT_NUMBER || 5000;
+let portNumber = process.env.PORT || 5000;
 app.listen(portNumber, () => console.log(`listning on port ${portNumber}`));
